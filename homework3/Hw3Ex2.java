256dc9883aebe93794a25a12a5558173ac06c76d
eshumeyko <tierdal@hotmail.com>
2019-03-25 12:10:40 -0500

    added array check for winning numbers
    rewrote some code to polish out the bugs
    
    no longer will roll duplicate numbers or zeroes
    
    will do a proper check for each number
    
    could be cleaned up more

--- homework3/Hw3Ex2.java
+++ homework3/Hw3Ex2.java
@@ -21,14 +21,14 @@ public class Hw3Ex2 {
         System.out.print("Do you want to roll until match (WARNING - MIGHT TAKE A LONG TIME) [True | False]: ");
         boolean roll_until_win = sc.nextBoolean();
 
-        Powerball Roll_1 = new Powerball();
-        Roll_1.roll_ball();
+        Powerball Roll = new Powerball();
+        Roll.roll_ball();
 
-        if (roll_until_win == false) {
-            System.out.println("Winning Numbers: " + Roll_1.ball_1 + " " + Roll_1.ball_2 + " " + Roll_1.ball_3 + " " + Roll_1.ball_4 + " " + Roll_1.ball_5 + " Powerball: " + Roll_1.powerball);
+        if (!roll_until_win) {
+            System.out.println("Winning Numbers: " + Roll.ball_1 + " " + Roll.ball_2 + " " + Roll.ball_3 + " " + Roll.ball_4 + " " + Roll.ball_5 + " Powerball: " + Roll.powerball);
         }
 
-        Roll_1.play(play_1,play_2,play_3,play_4,play_5,play_powerball,roll_until_win);
+        Roll.play(play_1,play_2,play_3,play_4,play_5,play_powerball,roll_until_win);
 
     }
 }
--- homework3/Powerball.java
+++ homework3/Powerball.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList; //https://stackoverflow.com/questions/9342859/i-need-to-find-a-integer-data-in-arraylist
 import java.util.Random;
+import java.util.Arrays; //https://study.com/academy/lesson/how-to-sort-an-array-in-java.html
+
 public class Powerball {
     //properties
 
@@ -13,85 +16,119 @@ public class Powerball {
     public int powerball;
 
     public void play(int play_1,int play_2,int play_3,int play_4,int play_5,int play_powerball, boolean roll_until_win){
-        boolean play_result_1 = false,play_result_2 = false,play_result_3 = false,play_result_4 = false,play_result_5 = false,play_result_powerball = false,win = false;
+        boolean play_result_1 = false,play_result_2 = false,play_result_3 = false,play_result_4 = false,play_result_5 = false,win_powerball = false,win = false;
         String winning_numbers = "",winning_message_numbers = "";
         int win_counter = 0, roll_counter = 0;
 
         System.out.println("Your Numbers: " + play_1 + " " + play_2 + " " + play_3 + " " + play_4 + " " + play_5 + " Powerball: " + play_powerball);
 
-        if (roll_until_win == false) {
-            if (ball_1 == play_1 || ball_1 == play_2 || ball_1 == play_3 || ball_1 == play_4 || ball_1 == play_5) {
+        //build two arrays
+        int [] ball_array = new int[5];
+        int [] play_array = new int[5];
+
+        play_array[0] = play_1;
+        play_array[1] = play_2;
+        play_array[2] = play_3;
+        play_array[3] = play_4;
+        play_array[4] = play_5;
+        ball_array[0] = ball_1;
+        ball_array[1] = ball_2;
+        ball_array[2] = ball_3;
+        ball_array[3] = ball_4;
+        ball_array[4] = ball_5;
+
+        Arrays.sort(play_array);
+        Arrays.sort(ball_array);
+
+        if (!roll_until_win) {
+            //roll - single play
+
+            ArrayList<Integer> ball_list = new ArrayList<>(); //https://stackoverflow.com/questions/9342859/i-need-to-find-a-integer-data-in-arraylist
+            ball_list.add(ball_array[0]);
+            ball_list.add(ball_array[1]);
+            ball_list.add(ball_array[2]);
+            ball_list.add(ball_array[3]);
+            ball_list.add(ball_array[4]);
+
+            if (ball_list.contains(play_array[0])) {
                 play_result_1 = true;
-                winning_message_numbers = winning_message_numbers + Integer.toString(ball_1);
+                winning_message_numbers = winning_message_numbers + Integer.toString(play_array[0]);
                 win_counter++;
             }
             if (play_result_1) {winning_message_numbers = winning_message_numbers + " ";}
-            if (ball_2 == play_1 || ball_2 == play_2 || ball_2 == play_3 || ball_2 == play_4 || ball_2 == play_5) {
+            if (ball_list.contains(play_array[1])) {
                 play_result_2 = true;
-                winning_message_numbers = winning_message_numbers + Integer.toString(ball_1);
+                winning_message_numbers = winning_message_numbers + Integer.toString(play_array[1]);
                 win_counter++;
             }
             if (play_result_2) {winning_message_numbers = winning_message_numbers + " ";}
-            if (ball_3 == play_1 || ball_3 == play_2 || ball_3 == play_3 || ball_3 == play_4 || ball_3 == play_5) {
+            if (ball_list.contains(play_array[2])) {
                 play_result_3 = true;
-                winning_message_numbers = winning_message_numbers + Integer.toString(ball_1);
+                winning_message_numbers = winning_message_numbers + Integer.toString(play_array[2]);
                 win_counter++;
             }
             if (play_result_3) {winning_message_numbers = winning_message_numbers + " ";}
-            if (ball_4 == play_1 || ball_4 == play_2 || ball_4 == play_3 || ball_4 == play_4 || ball_4 == play_5) {
+            if (ball_list.contains(play_array[3])) {
                 play_result_4 = true;
-                winning_message_numbers = winning_message_numbers + Integer.toString(ball_1);
+                winning_message_numbers = winning_message_numbers + Integer.toString(play_array[3]);
                 win_counter++;
             }
             if (play_result_4) {winning_message_numbers = winning_message_numbers + " ";}
-            if (ball_5 == play_1 || ball_5 == play_2 || ball_5 == play_3 || ball_5 == play_4 || ball_5 == play_5) {
+            if (ball_list.contains(play_array[4])) {
                 play_result_5 = true;
-                winning_message_numbers = winning_message_numbers + Integer.toString(ball_1);
+                winning_message_numbers = winning_message_numbers + Integer.toString(play_array[4]);
                 win_counter++;
             }
             if (play_result_5) {winning_message_numbers = winning_message_numbers + " ";}
             if (powerball == play_powerball) {
-                play_result_powerball = true;
-                winning_message_numbers = winning_message_numbers + "Powerball: " + Integer.toString(ball_1);
+                win_powerball = true;
+                winning_message_numbers = winning_message_numbers + "Powerball: " + Integer.toString(powerball);
                 win_counter++;
             }
 
-            if (play_result_1 || play_result_2 || play_result_3 || play_result_4 || play_result_5 || play_result_powerball) {
+            if (play_result_1 || play_result_2 || play_result_3 || play_result_4 || play_result_5 || win_powerball) {
                 winning_numbers = "Congratulations, you matched " + win_counter + " number(s)! Your matching number(s) are: " + winning_message_numbers;
                 System.out.println(winning_numbers);
             } else {
                 System.out.println("Sorry, you didn't win. Better luck next time!");
             }
-        } else if (roll_until_win == true) {
-            while (win == false) {
+        } else if (roll_until_win) {
+            //roll - until you win
+
+            while (!win) {
                 roll_ball();
                 roll_counter++;
-                if (ball_1 == play_1 || ball_1 == play_2 || ball_1 == play_3 || ball_1 == play_4 || ball_1 == play_5) {
+
+                ball_array[0] = ball_1;
+                ball_array[1] = ball_2;
+                ball_array[2] = ball_3;
+                ball_array[3] = ball_4;
+                ball_array[4] = ball_5;
+
+                Arrays.sort(ball_array);
+
+                if (ball_array[0] == play_array[0]) {
                     play_result_1 = true;
-                    win_counter++;
-                }
-                if (ball_2 == play_1 || ball_2 == play_2 || ball_2 == play_3 || ball_2 == play_4 || ball_2 == play_5) {
+                } else {play_result_1 = false;}
+                if (ball_array[1] == play_array[1]) {
                     play_result_2 = true;
-                    win_counter++;
-                }
-                if (ball_3 == play_1 || ball_3 == play_2 || ball_3 == play_3 || ball_3 == play_4 || ball_3 == play_5) {
+                } else {play_result_2 = false;}
+                if (ball_array[2] == play_array[2]) {
                     play_result_3 = true;
-                    win_counter++;
-                }
-                if (ball_4 == play_1 || ball_4 == play_2 || ball_4 == play_3 || ball_4 == play_4 || ball_4 == play_5) {
+                } else {play_result_3 = false;}
+                if (ball_array[3] == play_array[3]) {
                     play_result_4 = true;
-                    win_counter++;
-                }
-                if (ball_5 == play_1 || ball_5 == play_2 || ball_5 == play_3 || ball_5 == play_4 || ball_5 == play_5) {
+                } else {play_result_4 = false;}
+                if (ball_array[4] == play_array[4]) {
                     play_result_5 = true;
-                    win_counter++;
-                }
+                } else {play_result_5 = false;}
                 if (powerball == play_powerball) {
-                    play_result_powerball = true;
-                    win_counter++;
-                }
-                if (play_result_1 && play_result_2 && play_result_3 && play_result_4 && play_result_5 && play_result_powerball) {
-                    System.out.println("Congratulations, you finally won and matched all numbers! It took " + win_counter + " rolls!");
+                    win_powerball = true;
+                } else {win_powerball = false;}
+
+                if (play_result_1 && play_result_2 && play_result_3 && play_result_4 && play_result_5 && win_powerball) {
+                    System.out.println("Winning Numbers: " + ball_1 + " " + ball_2 + " " + ball_3 + " " + ball_4 + " " + ball_5 + " Powerball: " + powerball);
+                    System.out.println("Congratulations, you finally won and matched all numbers! It took " + roll_counter + " rolls!");
                     win = true;
                 }
             }
@@ -99,12 +136,41 @@ public class Powerball {
     }
 
     public void roll_ball(){
+        boolean ball_1_roll = false,ball_2_roll = false,ball_3_roll = false,ball_4_roll = false,ball_5_roll = false,powerball_roll = false;
+
         Random rnd = new Random();
-        ball_1 = rnd.nextInt(69);
-        ball_2 = rnd.nextInt(69);
-        ball_3 = rnd.nextInt(69);
-        ball_4 = rnd.nextInt(69);
-        ball_5 = rnd.nextInt(69);
+
+        while (!ball_1_roll) {
+            ball_1 = rnd.nextInt(69);
+            if (ball_1 > 0) {
+                ball_1_roll = true;
+            }
+        }
+        while (!ball_2_roll) {
+            ball_2 = rnd.nextInt(69);
+            if (ball_2 > 0 && ball_2 != ball_1) {
+                ball_2_roll = true;
+            }
+        }
+        while (!ball_3_roll) {
+            ball_3 = rnd.nextInt(69);
+            if (ball_3 > 0 && ball_3 != ball_1 && ball_3 != ball_2) {
+                ball_3_roll = true;
+            }
+        }
+        while (!ball_4_roll) {
+            ball_4 = rnd.nextInt(69);
+            if (ball_4 > 0 && ball_4 != ball_1 && ball_4 != ball_2 && ball_4 != ball_3) {
+                ball_4_roll = true;
+            }
+        }
+        while (!ball_5_roll) {
+            ball_5 = rnd.nextInt(69);
+            if (ball_5 > 0 && ball_5 != ball_1 && ball_5 != ball_2 && ball_5 != ball_3 && ball_5 != ball_4) {
+                ball_5_roll = true;
+            }
+        }
         powerball = rnd.nextInt(26);
     }
 }
+
