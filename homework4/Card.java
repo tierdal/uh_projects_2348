ff21fd062fced718dddafef92c2d13784d539420
eshumeyko <tierdal@hotmail.com>
2019-04-01 14:40:01 -0500

    Create homework4 and all the classes

---
+++ homework4/Card.java
@@ -0,0 +1,4 @@
+public class Card {
+    public int value; //1 - 13, 1 = Ace, 11 = Jack, 12 = Queen, 13 = King
+    public String suit; // H = Hearts, S = Spades, D = Diamonds, C = Clubs
+}
---
+++ homework4/Hw4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList; //https://stackoverflow.com/questions/9342859/i-need-to-find-a-integer-data-in-arraylist
+import java.util.Random;
+import java.util.Arrays; //https://study.com/academy/lesson/how-to-sort-an-array-in-java.html
+
+public class Hw4 {
+    public static void main(String[] args) {
+        //do stuff
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}
