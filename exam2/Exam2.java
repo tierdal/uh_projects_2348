33c89c68bf6f6de666fb8576f01a442d22ea9975
tierdal <tierdal@hotmail.com>
2019-04-13 11:18:51 -0500

    exam2 added output function

--- exam2/Exam2.java
+++ exam2/Exam2.java
@@ -17,11 +17,14 @@ public class Exam2 {
                     student_name_first = "FirstName_c" + counter_classroom + "s" + student_counter;
                     student_name_last = "LastName_c" + counter_classroom + "s" + student_counter;
                     school[counter_classroom - 1][counter_student - 1] = new Student(student_name_first, student_name_last);
-                    System.out.print(school[counter_classroom - 1][counter_student - 1].getStudentID());
-                    System.out.print(" : ");
-                    System.out.println(school[counter_classroom - 1][counter_student - 1].getStudentName());
+                    outputSchool(school[counter_classroom - 1][counter_student - 1].getStudentID(),school[counter_classroom - 1][counter_student - 1].getStudentName());
                 }
             }
         }
+    }
+
+    private static void outputSchool(int id, String name) {
+
+        System.out.println(id + ", " + name);
     }
 }
